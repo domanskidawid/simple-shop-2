1836df7c3b0adfddddb6c8dbc5ef3c8fd6bff0cf
domanskidawid <domanski.dawid@gmail.com>
2025-02-02 09:04:16 +0100

    Init

--- src/main/kotlin/com/dawid/test/HelloController.kt
+++ src/main/kotlin/com/dawid/test/HelloController.kt
@@ -8,6 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping
 class HelloController {
     @GetMapping("/")
     fun test(): ResponseEntity<String> {
-        return ResponseEntity.ok("Jest cool!")
+        return ResponseEntity.ok("Normalny Hello world")
+    }
+
+    @GetMapping("/users")
+    fun users(): ResponseEntity<User> {
+        return ResponseEntity.ok(User("Tomasz", "Aktuszer tobiasz"))
+    }
+    @GetMapping("/products")
+    fun products(): ResponseEntity<User> {
+        return ResponseEntity.ok(User("products", "Chuj products"))
     }
 }
+data class User(val name: String, val surname: String)
